9d0389a1b0c9d2b2e2097ef87e1f68b9111e13df
Eren Yeager <ErenYeager80@gmail.com>
2021-06-16 16:13:11 +0430

    start ui

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         String Str = "for 100 ";
         String Str1 = "end   for";
         String Str2 = "int x =10  ";
-        System
+
         System.out.println(Str.matches("for\\s+\\d+\\s+"));
         System.out.println(Str1.matches("\\s*end\\s+for\\s*"));
         System.out.println(Str2.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
---
+++ src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
