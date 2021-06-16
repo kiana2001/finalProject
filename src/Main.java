f6c361d8d1ffed7407b10a944ba15eeb10807ec7
kiana2001 <kianakermani1381@gmail.com>
2021-06-16 16:35:14 +0430

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+ static  String string;
     public static void Maches() {
-        String Str = "for 100 ";
-        String Str1 = "end   for";
-        String Str2 = "int x =10  ";
-        System
-        System.out.println(Str.matches("for\\s+\\d+\\s+"));
-        System.out.println(Str1.matches("\\s*end\\s+for\\s*"));
-        System.out.println(Str2.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
+        System.out.println(string.matches("for\\s+\\d+\\s+"));
+        System.out.println(string.matches("\\s*end\\s+for\\s*"));
+        System.out.println(string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
+        System.out.println(string.matches("[%]+"));
+        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+-]\\s*[\\w|\\d]"));
+        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[*-]\\s*[\\w|\\d]"));
+        System.out.println(string.matches("print\\s*[\\w+|\\d]+\\s*"));
+        //taghsim ro nemodom chejoori mishe rejex
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        string= scanner.nextLine();
         Maches();
     }
 }
