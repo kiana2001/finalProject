9ecceb99cb1788d6d841a372994b8fcd2c646930
kiana2001 <kianakermani1381@gmail.com>
2021-06-16 17:35:09 +0430

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
- static  String string;
-    public static void Maches() {
-        System.out.println(string.matches("for\\s+\\d+\\s+"));
+
+    public static void Maches(String string) {
+        System.out.println(string.matches("for\\s+\\d+\\s*"));
         System.out.println(string.matches("\\s*end\\s+for\\s*"));
         System.out.println(string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
+        System.out.println(string.matches("\\s*(int|float)\\s*\\w+\\s*"));
         System.out.println(string.matches("[%]+"));
-        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+-]\\s*[\\w|\\d]"));
-        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[*-]\\s*[\\w|\\d]"));
+        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+|-]\\s*[\\w|\\d]"));
+        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]"));
         System.out.println(string.matches("print\\s*[\\w+|\\d]+\\s*"));
-        //taghsim ro nemodom chejoori mishe rejex
+        System.out.println(string.matches("\\w+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]+"));
+        System.out.println(string.matches("\\s*\\w+\\s*[=]\\s*[\\w|\\d]+\\s*"));
+        System.out.println(string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]"));
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        string= scanner.nextLine();
-        Maches();
+
+        Maches(scanner.nextLine());
     }
 }
