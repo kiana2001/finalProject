2497af85cc7b7a12bc084487eed2240b5d1aff62
kiana2001 <kianakermani1381@gmail.com>
2021-06-16 12:00:41 +0430

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void  Maches(){
+        String Str = "for 6 ";
+
+        System.out.print("Does String contains regex for\\s\\d ? : " );
+        System.out.println(Str.matches("for\\s\\d\\s"));
+
+    }
+    public static void main(String[] args) {
+        Maches();
     }
 }
