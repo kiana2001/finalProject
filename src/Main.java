640053054978e7c6b546e10d169af1b86368951b
kiana2001 <kianakermani1381@gmail.com>
2021-06-16 13:38:21 +0430

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
 
-    public static void  Maches(){
-        String Str = "for 6 ";
-
-        System.out.print("Does String contains regex for\\s\\d ? : " );
-        System.out.println(Str.matches("for\\s\\d\\s"));
-
+    public static void Maches() {
+        String Str = "for 100 ";
+        String Str1 = "end   for";
+        String Str2 = "int x =10  ";
+        System
+        System.out.println(Str.matches("for\\s+\\d+\\s+"));
+        System.out.println(Str1.matches("\\s*end\\s+for\\s*"));
+        System.out.println(Str2.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
     }
+
     public static void main(String[] args) {
         Maches();
     }
