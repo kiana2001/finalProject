86bcf77012a910c1920eb7b6c07e6854fca88d63
kiana2001 <kianakermani1381@gmail.com>
2021-06-17 15:39:12 +0430

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,85 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int count=1;
 
     public static void Maches(String string) {
-        System.out.println(string.matches("for\\s+\\d+\\s*"));
-        System.out.println(string.matches("\\s*end\\s+for\\s*"));
-        System.out.println(string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*"));
-        System.out.println(string.matches("\\s*(int|float)\\s*\\w+\\s*"));
-        System.out.println(string.matches("[%]+"));
-        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+|-]\\s*[\\w|\\d]"));
-        System.out.println(string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]"));
-        System.out.println(string.matches("print\\s*[\\w+|\\d]+\\s*"));
-        System.out.println(string.matches("\\w+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]+"));
-        System.out.println(string.matches("\\s*\\w+\\s*[=]\\s*[\\w|\\d]+\\s*"));
-        System.out.println(string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]"));
+
+        if (string.matches("for\\s+\\d+\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+
+        if (string.matches("\\s*end\\s+for\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\s*(int|float)\\s*\\w+\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("[%]+")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+|-]\\s*[\\w|\\d]")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("print\\s*[\\w+|\\d]+\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\w+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]+")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\s*\\w+\\s*[=]\\s*[\\w|\\d]+\\s*")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]")) {
+            System.out.println(true);
+            System.out.println(count);
+        } else {
+            count++;
+        }
+        if (string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]")) {
+            System.out.println(true);
+            System.out.println(count);
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Maches(scanner.nextLine());
     }
 }
