4e7226dbd4a24d86b7b451e6fa122fd62bca641d
kiana2001 <kianakermani1381@gmail.com>
2021-06-18 14:10:51 +0430

    Enums

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,31 @@
+import javax.lang.model.element.ElementKind;
 import java.util.Scanner;
 
 public class Main {
-    public static int count=1;
-
-    public static void Maches(String string) {
-
-        if (string.matches("for\\s+\\d+\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
+    public static int count = 1;
 
 
-    public static void Maches() {
-    
-        if (string.matches("\\s*end\\s+for\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\s*(int|float)\\s*\\w+\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
+    public static Enum Maches(String string) {
+        if (string.matches("\\s*for\\s*\\d+\\s*")) return Parts.FOR_HEAD;
+        if (string.matches("\\s*end\\s+for\\s*")) return Parts.END_FOR;
+        if (string.matches("\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*")) return Parts.DEFINE_VARIABLE_WITH_AMOUNT;
+        if (string.matches("\\s*(int|float)\\s*\\w+\\s*")) return Parts.DEFINE_VARIABLE_WITHOUT_AMOUNT;
         if (string.matches("[%]+")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
+            return Parts.DIVIDER;
         }
-        if (string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[+|-]\\s*[\\w|\\d]")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("print\\s*[\\w+|\\d]+\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\w+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]+")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\s*\\w+\\s*[=]\\s*[\\w|\\d]+\\s*")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]")) {
-            System.out.println(true);
-            System.out.println(count);
-        } else {
-            count++;
-        }
-        if (string.matches("\\w+\\s*[=]\\s*[\\-*\\/+]")) {
-            System.out.println(true);
-            System.out.println(count);
+        if (string.matches("[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]"))
+            return Parts.MATH_OPERATION;
+        if (string.matches("print\\s*[\\w+|\\d]+\\s*"))
+            return Parts.PRINT;
+        if (string.matches("\\w+\\s*[=]\\s*[\\w|\\d]+\\s*([\\-*\\/+]\\s*[\\w|\\d])*")) {
+            return Parts.ASSIGNEMNT;
         }
 
+        return null;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Maches(scanner.nextLine());
+        System.out.println(Maches(scanner.nextLine()));
     }
 }
