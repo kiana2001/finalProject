a6cbdbed7ab8f9d2dfc54124ea78f26c1e9df848
Eren Yeager <ErenYeager80@gmail.com>
2021-06-25 01:56:52 +0430

    Improve syntax of match methode

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,15 +1,14 @@
 public class Interpreter {
-    public static Enum Maches(String string) {
+
+    public static Parts getType(String string) {
         if (string.matches(Pattern.FOR_HEAD)) return Parts.FOR_HEAD;
         if (string.matches(Pattern.FOR_END)) return Parts.END_FOR;
         if (string.matches(Pattern.VARIABLE_WITH_AMOUNT)) return Parts.DEFINE_VARIABLE_WITH_AMOUNT;
         if (string.matches(Pattern.VARIABLE_WITHOUT_AMOUNT)) return Parts.DEFINE_VARIABLE_WITHOUT_AMOUNT;
         if (string.matches(Pattern.DIVIDER)) return Parts.DIVIDER;
-
         if (string.matches(Pattern.MATH_OPERATION)) return Parts.MATH_OPERATION;
         if (string.matches(Pattern.PRINT)) return Parts.PRINT;
         if (string.matches(Pattern.ASSIGNMENT))  return Parts.ASSIGNEMNT;
-
 
         return null;
     }
