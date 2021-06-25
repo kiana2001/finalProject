825f1c8f5bb32ece4d6a40f8a29e3498c70f9e26
Eren Yeager <ErenYeager80@gmail.com>
2021-06-25 21:57:09 +0430

    improve file tree

--- src/ui/sections/FileTree.java
+++ src/ui/sections/FileTree.java
@@ -1,8 +1,80 @@
 package ui.sections;
 
 
-import javafx.scene.control.TreeView;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FileTree extends TreeView<FileView> {
+    private final List<String> files = new ArrayList<>();
+    private final TreeItem<FileView> rootItem;
 
+    public FileTree() {
+
+        rootItem = new TreeItem<>(new FileView("test"), new ImageView(new Image("ui/assets/folder.png")));
+        rootItem.setExpanded(true);
+
+        setRoot(rootItem);
+        setCellFactory(p -> new TextFieldTreeCellImpl());
+    }
+
+    public void addItem(FileView file){
+        TreeItem<FileView> item = new TreeItem<>(file, new ImageView(new Image(file.isFile()?"ui/assets/file.png":"ui/assets/folder.png")));
+        files.add(file.getPath());
+        rootItem.getChildren().add(item);
+    }
+
+    public List<String> getFiles() {
+        return files;
+    }
+
+    private static final class TextFieldTreeCellImpl extends TreeCell<FileView> {
+
+        private final ContextMenu addMenu = new ContextMenu();
+
+        public TextFieldTreeCellImpl() {
+            MenuItem addMenuItem = new MenuItem("Add Employee");
+            addMenu.getItems().add(addMenuItem);
+            addMenuItem.setOnAction(t -> {
+                TreeItem<FileView> newEmployee = new TreeItem<>(new FileView("Employee"));
+                getTreeItem().getChildren().add(newEmployee);
+            });
+        }
+
+
+        @Override
+        public void updateItem(FileView item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+
+                setText(getString());
+                setGraphic(getTreeItem().getGraphic());
+                if (!item.isFile())
+                    setContextMenu(addMenu);
+
+            }
+            setOnMouseClicked(e->{
+                if(e.getButton().equals(MouseButton.PRIMARY)){
+                    if(e.getClickCount() == 2){
+                        System.out.println(item.toString());
+                    }
+                }
+
+            });
+        }
+
+        private String getString() {
+            return getItem() == null ? "" : getItem().toString();
+        }
+    }
 }
