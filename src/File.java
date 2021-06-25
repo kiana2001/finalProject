628cc5dc3a897b003a8784bcbe35b3b088a7cf6a
kiana2001 <kianakermani1381@gmail.com>
2021-06-25 19:53:24 +0430

    read string

--- src/File.java
+++ src/File.java
@@ -1,45 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.io.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 
-
 public class File {
-    public static Parts getType(String string) {
-
-        if (string.matches(Pattern.FOR_HEAD)) return Parts.FOR_HEAD;
-        if (string.matches(Pattern.FOR_END)) return Parts.END_FOR;
-        if (string.matches(Pattern.VARIABLE_WITH_AMOUNT)) return Parts.DEFINE_VARIABLE_WITH_AMOUNT;
-        if (string.matches(Pattern.VARIABLE_WITHOUT_AMOUNT)) return Parts.DEFINE_VARIABLE_WITHOUT_AMOUNT;
-        if (string.matches(Pattern.DIVIDER)) return Parts.DIVIDER;
-        if (string.matches(Pattern.MATH_OPERATION)) return Parts.MATH_OPERATION;
-        if (string.matches(Pattern.PRINT)) return Parts.PRINT;
-        if (string.matches(Pattern.ASSIGNMENT))  return Parts.ASSIGNEMNT;
+    public static Parts getType(String data) {
+        String FOR_HEAD = "\\s*for\\s*\\d+\\s*";
+        String FOR_END = "\\s*end\\s+for\\s*";
+        String VARIABLE_WITH_AMOUNT = "\\s*(int|float)\\s*\\w+\\s*[=]\\s*\\d+\\s*";
+        String VARIABLE_WITHOUT_AMOUNT = "\\s*(int|float)\\s*\\w+\\s*";
+        String DIVIDER = "[%]+";
+        String MATH_OPERATION = "[\\w|\\d]+\\s*[=]\\s*[\\w|\\d]+\\s*[\\-*\\/+]\\s*[\\w|\\d]";
+        String PRINT = "print\\s*[\\w+|\\d]+\\s*";
+        String ASSIGNMENT = "\\w+\\s*[=]\\s*[\\w|\\d]+\\s*([\\-*\\/+]\\s*[\\w|\\d])*";
+        if (data.matches(Pattern.FOR_HEAD)) return Parts.FOR_HEAD;
+        if (data.matches(Pattern.FOR_END)) return Parts.END_FOR;
+        if (data.matches(Pattern.VARIABLE_WITH_AMOUNT)) return Parts.DEFINE_VARIABLE_WITH_AMOUNT;
+        if (data.matches(Pattern.VARIABLE_WITHOUT_AMOUNT)) return Parts.DEFINE_VARIABLE_WITHOUT_AMOUNT;
+        if (data.matches(Pattern.DIVIDER)) return Parts.DIVIDER;
+        if (data.matches(Pattern.MATH_OPERATION)) return Parts.MATH_OPERATION;
+        if (data.matches(Pattern.PRINT)) return Parts.PRINT;
+        if (data.matches(Pattern.ASSIGNMENT)) return Parts.ASSIGNEMNT;
 
         return null;
     }
+   static String data =
+            "int x = 10" + "\n" +
+                    "int y = 30" + "\n" +
+                    "int z" + "\n" +
+                    "int num = 1" + "\n" +
+                    "" + "\n" +
+                    "%%" + "\n" +
+                    "" + "\n" +
+                    "for 100" + "\n" +
+                    "   print num" + "\n" +
+                    "   num = num +1" + "\n" +
+                    "end for" + "\n" +
+                    "for 100" + "\n" +
+                    "   print num" + "\n" +
+                    "   num = num +1" + "\n" +
+                    "end for" + "\n" +
+                    "for 100" + "\n" +
+                    "   print num" + "\n" +
+                    "   num = num +1" + "\n" +
+                    "end for" + "\n" +
+                    "for 100" + "\n" +
+                    "   print num" + "\n" +
+                    "   num = num +1" + "\n" +
+                    "end for";
+
     public static void main(String[] args) throws IOException {
-        String data = "  \"abstract\", \"assert\", \"boolean\", \"break\", \"byte\",\n" +
-                "            \"case\", \"catch\", \"char\", \"class\", \"const\",\n" +
-                "            \"continue\", \"default\", \"do\", \"double\", \"else\",\n" +
-                "            \"enum\", \"extends\", \"final\", \"finally\", \"float\",\n" +
-                "            \"for\", \"goto\", \"if\", \"implements\", \"import\",\n" +
-                "            \"instanceof\", \"int\", \"interface\", \"long\", \"native\",\n" +
-                "            \"new\", \"package\", \"private\", \"protected\", \"public\",\n" +
-                "            \"return\", \"short\", \"static\", \"strictfp\", \"super\",\n" +
-                "            \"switch\", \"synchronized\", \"this\", \"throw\", \"throws\",\n" +
-                "            \"transient\", \"try\", \"void\", \"volatile\", \"while\",\"end\",\"print\"";
+
+
         Reader reader = new StringReader(data);
         BufferedReader input = new BufferedReader(reader);
         while (true) {
             var line = new AtomicReference<>(input.readLine());
-            if(line.get() == null){
+            if (line.get() == null) {
                 break;
             }
             getType(data);
+            System.out.println(getType(data));
         }
-        System.out.println(getType(data));
     }
 }
+
+
+
